dc5c141d6e9b511f8be89255db0bb6029989a409
Hays Chan, aka Nozomi <25737801+hayschan@users.noreply.github.com>
2024-11-15 20:01:09 +0700

    Comment out Nordic Logo

--- src/components/Brands/brandsData.tsx
+++ src/components/Brands/brandsData.tsx
@@ -2,12 +2,26 @@ import { Brand } from "@/types/brand";
 
 const brandsData: Brand[] = [
   {
-    id: 1,
-    name: "Nordic Semiconductor",
-    href: "https://lora-alliance.org/",
-    image: "/images/brands/nordic-semiconductor.svg",
-    imageLight: "/images/brands/nordic-semiconductor.svg",
+    id: 6,
+    name: "Meshtastic",
+    href: "https://meshtastic.org/",
+    image: "https://raw.githubusercontent.com/meshtastic/design/refs/heads/master/logo/logo.svg",
+    imageLight: "https://raw.githubusercontent.com/meshtastic/design/refs/heads/master/logo/logo.svg",
   },
+  {
+    id: 7,
+    name: "LoRa",
+    href: "https://lora-alliance.org/",
+    image: "/images/brands/lorawanicon.svg",
+    imageLight: "/images/brands/lorawanicon.svg",
+  },
+  // {
+  //   id: 1,
+  //   name: "Nordic Semiconductor",
+  //   href: "https://lora-alliance.org/",
+  //   image: "/images/brands/nordic-semiconductor.svg",
+  //   imageLight: "/images/brands/nordic-semiconductor.svg",
+  // },
   {
     id: 2,
     name: "Raspberry Pi",
@@ -22,33 +36,19 @@ const brandsData: Brand[] = [
     image: "/images/brands/espressif-systems.svg",
     imageLight: "/images/brands/espressif-systems.svg",
   },
-  {
-    id: 4,
-    name: "WiFi",
-    href: "https://lora-alliance.org/",
-    image: "/images/brands/wifi.svg",
-    imageLight: "/images/brands/wifi.svg",
-  },
+  // {
+  //   id: 4,
+  //   name: "WiFi",
+  //   href: "https://lora-alliance.org/",
+  //   image: "/images/brands/wifi.svg",
+  //   imageLight: "/images/brands/wifi.svg",
+  // },
   {
     id: 5,
     name: "BlueTooth",
     href: "https://lora-alliance.org/",
     image: "/images/brands/bluetooth.svg",
     imageLight: "/images/brands/bluetooth.svg",
-  },
-  {
-    id: 6,
-    name: "Meshtastic",
-    href: "https://meshtastic.org/",
-    image: "https://raw.githubusercontent.com/meshtastic/design/refs/heads/master/logo/logo.svg",
-    imageLight: "https://raw.githubusercontent.com/meshtastic/design/refs/heads/master/logo/logo.svg",
-  },
-  {
-    id: 7,
-    name: "LoRa",
-    href: "https://lora-alliance.org/",
-    image: "/images/brands/lorawanicon.svg",
-    imageLight: "/images/brands/lorawanicon.svg",
   },
 ];
 
