65a02ce3dea358387adc4b1d98b1836cf3a0dd48
Hays Chan, aka Nozomi <25737801+hayschan@users.noreply.github.com>
2024-11-15 19:56:08 +0700

    Fix brand typo

--- src/components/Brands/brandsData.tsx
+++ src/components/Brands/brandsData.tsx
@@ -2,28 +2,21 @@ import { Brand } from "@/types/brand";
 
 const brandsData: Brand[] = [
   {
-    id: 8,
+    id: 1,
     name: "Nordic Semiconductor",
     href: "https://lora-alliance.org/",
     image: "/images/brands/nordic-semiconductor.svg",
     imageLight: "/images/brands/nordic-semiconductor.svg",
   },
   {
-    id: 7,
+    id: 2,
     name: "Raspberry Pi",
     href: "https://lora-alliance.org/",
     image: "/images/brands/raspberry-pi.svg",
     imageLight: "/images/brands/raspberry-pi.svg",
   },
   {
-    id: 6,
-    name: "Espressif Systems",
-    href: "https://lora-alliance.org/",
-    image: "/images/brands/espressif-systems.svg",
-    imageLight: "/images/brands/espressif-systems.svg",
-  },
-  {
-    id: 5,
+    id: 3,
     name: "Espressif Systems",
     href: "https://lora-alliance.org/",
     image: "/images/brands/espressif-systems.svg",
@@ -37,21 +30,21 @@ const brandsData: Brand[] = [
     imageLight: "/images/brands/wifi.svg",
   },
   {
-    id: 3,
+    id: 5,
     name: "BlueTooth",
     href: "https://lora-alliance.org/",
     image: "/images/brands/bluetooth.svg",
     imageLight: "/images/brands/bluetooth.svg",
   },
   {
-    id: 2,
+    id: 6,
     name: "Meshtastic",
     href: "https://meshtastic.org/",
     image: "https://raw.githubusercontent.com/meshtastic/design/refs/heads/master/logo/logo.svg",
     imageLight: "https://raw.githubusercontent.com/meshtastic/design/refs/heads/master/logo/logo.svg",
   },
   {
-    id: 1,
+    id: 7,
     name: "LoRa",
     href: "https://lora-alliance.org/",
     image: "/images/brands/lorawanicon.svg",
