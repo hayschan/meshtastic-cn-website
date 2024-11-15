d08a1ee1885264ae8f79af857f2d359d36c66153
Hays Chan, aka Nozomi <25737801+hayschan@users.noreply.github.com>
2024-11-15 20:52:13 +0700

    Change dog photo to compressed version

--- src/components/About/AboutSectionOne.tsx
+++ src/components/About/AboutSectionOne.tsx
@@ -20,13 +20,13 @@ const AboutSectionOne = () => {
             <div className="w-full px-4 lg:w-1/2">
               <div className="relative mx-auto aspect-[25/24] max-w-[500px] lg:mr-0">
                 <Image
-                  src="/images/about/prototype-t1000-e-dog-harness-enclosure.webp"
+                  src="/images/about/prototype-t1000-e-dog-harness-enclosure-squoosh.webp"
                   alt="Meshtastic Introduction"
                   fill
                   className="mx-auto max-w-full drop-shadow-three dark:hidden dark:drop-shadow-none lg:mr-0"
                 />
                 <Image
-                  src="/images/about/prototype-t1000-e-dog-harness-enclosure.webp"
+                  src="/images/about/prototype-t1000-e-dog-harness-enclosure-squoosh.webp"
                   alt="Meshtastic Introduction"
                   fill
                   className="mx-auto hidden max-w-full drop-shadow-three dark:block dark:drop-shadow-none lg:mr-0"
