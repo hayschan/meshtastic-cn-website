d75ecea4d4ac088ea5c10be20827ff2b67d89801
Hays Chan, aka Nozomi <25737801+hayschan@users.noreply.github.com>
2024-11-15 21:30:28 +0700

    Add a margin between About Section One

--- src/components/About/AboutSectionOne.tsx
+++ src/components/About/AboutSectionOne.tsx
@@ -13,7 +13,7 @@ const AboutSectionOne = () => {
                 paragraph="Meshtastic 是一个开源的 LoRa 网状网络项目，允许用户在没有互联网、手机信号或电力的情况下进行通信。无论是在户外探险、自然灾害期间，还是在远程地区，Meshtastic 都能提供去中心化的通信解决方案。"
                 mb="44px"
               />
-              <p className="text-base text-body-color">
+              <p className="text-base text-body-color mb-4">
                 Meshtastic 是完全社区驱动的项目，其目标是通过低成本的硬件和开源软件，为全球用户提供易用的长距离通信平台。它通过 LoRa 技术构建网状网络，用户设备会自动中继消息，实现区域覆盖。
               </p>
               <p className="text-base text-body-color">
